da29516f2a2cef7708fe6754b168cb64a3522088
Algian Aquillo <algian.aquill@gmail.com>
2026-05-17 00:16:21 +0800

    Initial commit: UGNAY backend and frontend setup

---
+++ backend/src/test/java/com/ugnay/ugnay/UgnayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugnay.ugnay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UgnayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
